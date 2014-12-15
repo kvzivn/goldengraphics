dc01dc89912d779077e44c650e329f798356568c
Hampus Persson <hampus.persson@apsis.com>
2014-12-15 09:24:18 +0100

    Update styles task

--- gulp/styles.js
+++ gulp/styles.js
@@ -1,26 +1,41 @@
-;(function() {
+;(function gulpStyles() {
     'use strict';
 
-    var gulp = require('gulp');
-    var browserSync = require('browser-sync');
-
-    var $ = require('gulp-load-plugins')({
-        pattern: ['gulp-*', 'main-bower-files']
-    });
+    var gulp = require('gulp'),
+        $ = require('gulp-load-plugins')({
+            pattern: ['gulp-*', 'main-bower-files']
+        });
 
 
-    gulp.task('build', function() {
-        gulp.start('html');
-    });
-
-
-    gulp.task('styles', function() {
+    gulp.task('styles', ['scss-lint'], function styles() {
         return gulp.src('stylesheets/styles.scss')
             .pipe($.rubySass())
             .pipe($.autoprefixer('last 2 version'))
-            .pipe(scsslint())
             .pipe(gulp.dest('stylesheets'))
             .pipe($.size());
     });
+
+    gulp.task('scss-lint', function scssLint() {
+        gulp.src('stylesheets/**/*.scss')
+            .pipe($.cached('scsslint'))
+            .pipe($.scssLint({
+                customReport: myCustomReporter
+            }))
+            .on('error', $.notify.onError(function onError(error) {
+                return error.message;
+            }));
+    });
+
+    function myCustomReporter(file) {
+        // Will report all issues, including warnings
+        if ( 0 < file.scsslint.issues.length  ) {
+            $.util.beep();
+            $.notify().write({
+                message: file.scsslint.issues.length + ' error found in ' + file.path
+            });
+        }
+
+    }
+
 
 }());
