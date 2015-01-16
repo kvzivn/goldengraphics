88cb004bac551b849afb0dca4c08a8a575505440
kvzivn <hello@kevinivan.com>
2015-01-16 09:45:32 +0100

    first

--- gulp/styles.js
+++ gulp/styles.js
@@ -7,35 +7,12 @@
         });
 
 
-    gulp.task('styles', ['scss-lint'], function styles() {
+    gulp.task('styles', function styles() {
         return gulp.src('stylesheets/styles.scss')
             .pipe($.rubySass())
             .pipe($.autoprefixer('last 2 version'))
             .pipe(gulp.dest('stylesheets'))
             .pipe($.size());
     });
-
-    gulp.task('scss-lint', function scssLint() {
-        gulp.src('stylesheets/**/*.scss')
-            .pipe($.cached('scsslint'))
-            .pipe($.scssLint({
-                customReport: myCustomReporter
-            }))
-            .on('error', $.notify.onError(function onError(error) {
-                return error.message;
-            }));
-    });
-
-    function myCustomReporter(file) {
-        // Will report all issues, including warnings
-        if ( 0 < file.scsslint.issues.length  ) {
-            $.util.beep();
-            $.notify().write({
-                message: file.scsslint.issues.length + ' error found in ' + file.path
-            });
-        }
-
-    }
-
 
 }());
